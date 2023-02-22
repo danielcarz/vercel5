54e60ea23e7a846073827b687f6df0b17a9dda96
danielcarz <dcz9412@gmail.com>
2023-02-22 18:28:56 -0500

    SASS positioning and styling

--- src/components/ubications.jsx
+++ src/components/ubications.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
+//IMAGES
+import map from '../assets/images/MAPA2.png'
+
 const Ubication = () => {
 
 
     return(
-        <h1>helloo Ubication</h1>
+        <section className="ubication_container">
+            <section className="ubication_info_wrapper">
+                <ul>
+                    <li>direccion 1</li>
+                    <li>direccion 1</li>
+                    <li>direccion 1</li>
+                </ul>
+            </section>
+
+            <figure className="ubication_image_wrapper">
+                <a href="">
+                    <img src={map} alt="ubication" /> 
+                </a>
+            </figure>
+        </section>
     )
 }
 
