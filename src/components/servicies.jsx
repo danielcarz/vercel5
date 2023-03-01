8fe5d348b51c84d28fc509dae0e13dde7ed825b3
danielcarz <dcz9412@gmail.com>
2023-03-01 17:21:18 -0500

    ASSETS: agregamos imagenes de los servicios

--- src/components/servicies.jsx
+++ src/components/servicies.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 //IMAGES
+//emoticons
 import emoticon from '../assets/emoticons/masaje-corporal.png'
+import reflexiologia from '../assets/emoticons/masaje-de-pies.png'
+import limpieza_espacios from '../assets/emoticons/spa-y-relax.png'
+import encuentros from '../assets/emoticons/spa.png'
+import masaje_relajante from '../assets/emoticons/masaje(1).png'
+import masoterapia from '../assets/emoticons/masaje(2).png'
+import terapiachina from '../assets/emoticons/piedra-caliente.png'
+import embarazo from '../assets/emoticons/masaje-corporal.png'
+
+//imagenes
+import reflex from '../assets/images/reflex.jpg'
+import alma from '../assets/images/alma.jpg'
+import reljante from '../assets/images/relajante.jpg'
+import cuencos from '../assets/images/cuencos.jpg'
+import tera_china from '../assets/images/terapia_china.jpg'
+import maso from '../assets/images/maso.jpg'
+
 import reiki from '../assets/images/IMG_6885.jpg'
 
 
@@ -14,14 +31,14 @@ const Servicies = () => {
                     <figure className="container_images_servicies">
                          <figure className="emoticons_servicies">
 
-                              <img src={emoticon} alt="emoticon" />
+                              <img src={reflexiologia} alt="emoticon" />
                     
                          </figure>
                     
                          <h2 className="subtittle">REFLEXIOLOGIA</h2>
 
                          <figure className="image_servicios_wrapper">
-                              <img src={reiki} alt="Servicios" />
+                              <img src={reflex } alt="Servicios" />
                          </figure>
 
                     </figure>
@@ -46,7 +63,7 @@ const Servicies = () => {
                     <figure className="container_images_servicies">
                          <figure className="emoticons_servicies">
 
-                              <img src={emoticon} alt="emoticon" />
+                              <img src={limpieza_espacios} alt="emoticon" />
                     
                          </figure>
                     
@@ -79,47 +96,14 @@ const Servicies = () => {
                     <figure className="container_images_servicies">
                          <figure className="emoticons_servicies">
 
-                              <img src={emoticon} alt="emoticon" />
-                    
-                         </figure>
-                    
-                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
-
-                         <figure className="image_servicios_wrapper">
-                              <img src={reiki} alt="Servicios" />
-                         </figure>
-
-                    </figure>
-                            
-                    <section className="container_info_cards">
-                         <div className="text_servicie_wrapper">
-                              <p>
-                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
-                              </p>
-                              <p className="prices_servicies">$ 100.000.00</p>
-                         </div>
-
-                         <div className="button_servicies_wrapper">
-                              <button className="button_servicies">Reserva</button>
-                         </div>
-
-                    </section>
-              
-               </section>
-
-
-               <section className="servicie_wrapper">
-                    <figure className="container_images_servicies">
-                         <figure className="emoticons_servicies">
-
-                              <img src={emoticon} alt="emoticon" />
+                              <img src={encuentros} alt="emoticon" />
                     
                          </figure>
                     
                          <h2 className="subtittle">ENCUENTROS PARA SANAR EL ALMA</h2>
 
                          <figure className="image_servicios_wrapper">
-                              <img src={reiki} alt="Servicios" />
+                              <img src={alma} alt="Servicios" />
                          </figure>
 
                     </figure>
@@ -145,14 +129,14 @@ const Servicies = () => {
                     <figure className="container_images_servicies">
                          <figure className="emoticons_servicies">
 
-                              <img src={emoticon} alt="emoticon" />
+                              <img src={masaje_relajante} alt="emoticon" />
                     
                          </figure>
                     
                          <h2 className="subtittle">MASAJE RELAJANTE</h2>
 
                          <figure className="image_servicios_wrapper">
-                              <img src={reiki} alt="Servicios" />
+                              <img src={reljante} alt="Servicios" />
                          </figure>
 
                     </figure>
@@ -185,7 +169,7 @@ const Servicies = () => {
                          <h2 className="subtittle">TERAPIA DE SONIDO CON CUENCOS TIBETANOS</h2>
 
                          <figure className="image_servicios_wrapper">
-                              <img src={reiki} alt="Servicios" />
+                              <img src={ cuencos} alt="Servicios" />
                          </figure>
 
                     </figure>
@@ -211,7 +195,7 @@ const Servicies = () => {
                     <figure className="container_images_servicies">
                          <figure className="emoticons_servicies">
 
-                              <img src={emoticon} alt="emoticon" />
+                              <img src={embarazo} alt="emoticon" />
                     
                          </figure>
                     
@@ -243,14 +227,14 @@ const Servicies = () => {
                     <figure className="container_images_servicies">
                          <figure className="emoticons_servicies">
 
-                              <img src={emoticon} alt="emoticon" />
+                              <img src={masoterapia} alt="emoticon" />
                     
                          </figure>
                     
                          <h2 className="subtittle">MASOTERAPIA CHINA</h2>
 
                          <figure className="image_servicios_wrapper">
-                              <img src={reiki} alt="Servicios" />
+                              <img src={maso} alt="Servicios" />
                          </figure>
 
                     </figure>
@@ -275,14 +259,14 @@ const Servicies = () => {
                     <figure className="container_images_servicies">
                          <figure className="emoticons_servicies">
 
-                              <img src={emoticon} alt="emoticon" />
+                              <img src={terapiachina} alt="emoticon" />
                     
                          </figure>
                     
                          <h2 className="subtittle">TERAPIA CHINA</h2>
 
                          <figure className="image_servicios_wrapper">
-                              <img src={reiki} alt="Servicios" />
+                              <img src={tera_china} alt="Servicios" />
                          </figure>
 
                     </figure>
