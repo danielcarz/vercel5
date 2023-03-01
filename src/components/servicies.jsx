aca36d402f53c5be17d8675e98247976a0c52a65
danielcarz <dcz9412@gmail.com>
2023-03-01 16:35:37 -0500

    SASS maquetacion

--- src/components/servicies.jsx
+++ src/components/servicies.jsx
@@ -8,42 +8,240 @@ import reiki from '../assets/images/IMG_6885.jpg'
 const Servicies = () => {
 
     return(
-        <section className="servicies_container">
-
-          <figure className="container_images_servicies">
-               <figure className="emoticons_servicies">
-
-                    <img src={emoticon} alt="emoticon" />
-           
-               </figure>
+          <section className="servicies_container">
           
-               <h2 className="subtittle">REFLEXIOLOGIA</h2>
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
 
-               <figure className="image_servicios_wrapper">
-                    <img src={reiki} alt="Servicios" />
-               </figure>
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
 
-          </figure>
-               
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
 
-               
-          <section className="container_info_cards">
-               <div className="text_servicie_wrapper">
-                    <p>
-                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
-                    </p>
-                    <p className="prices_servicies">$ 100.000.00</p>
-               </div>
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
 
-               <div className="button_servicies_wrapper">
-                    <button className="button_servicies">Reserva</button>
-               </div>
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
 
-          </section>
+                    </section>
               
-          
+               </section>
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+     
            
-        </section>
+          </section>
     )
 }
 
