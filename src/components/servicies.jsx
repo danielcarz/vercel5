3afba8fb43a0eb0389371f5af8ee5ea9b7a6b529
danielcarz <dcz9412@gmail.com>
2023-03-01 16:41:03 -0500

    SERVICIES: agregamos los servicios

--- src/components/servicies.jsx
+++ src/components/servicies.jsx
@@ -50,7 +50,7 @@ const Servicies = () => {
                     
                          </figure>
                     
-                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+                         <h2 className="subtittle">LIMPIEZA ENERGÉTICA DE ESPACIOS</h2>
 
                          <figure className="image_servicios_wrapper">
                               <img src={reiki} alt="Servicios" />
@@ -116,7 +116,7 @@ const Servicies = () => {
                     
                          </figure>
                     
-                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+                         <h2 className="subtittle">ENCUENTROS PARA SANAR EL ALMA</h2>
 
                          <figure className="image_servicios_wrapper">
                               <img src={reiki} alt="Servicios" />
@@ -149,7 +149,7 @@ const Servicies = () => {
                     
                          </figure>
                     
-                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+                         <h2 className="subtittle">MASAJE RELAJANTE</h2>
 
                          <figure className="image_servicios_wrapper">
                               <img src={reiki} alt="Servicios" />
@@ -182,7 +182,7 @@ const Servicies = () => {
                     
                          </figure>
                     
-                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+                         <h2 className="subtittle">TERAPIA DE SONIDO CON CUENCOS TIBETANOS</h2>
 
                          <figure className="image_servicios_wrapper">
                               <img src={reiki} alt="Servicios" />
@@ -215,7 +215,71 @@ const Servicies = () => {
                     
                          </figure>
                     
-                         <h2 className="subtittle">REFLEXIOLOGIA</h2>
+                         <h2 className="subtittle">REIKI EN EL EMBARAZO</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">MASOTERAPIA CHINA</h2>
+
+                         <figure className="image_servicios_wrapper">
+                              <img src={reiki} alt="Servicios" />
+                         </figure>
+
+                    </figure>
+                            
+                    <section className="container_info_cards">
+                         <div className="text_servicie_wrapper">
+                              <p>
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatibus at itaque dignissimos ipsam laborum nostrum ipsum sunt! Totam, inventore. Soluta at porro recusandae ullam cumque praesentium voluptatem iste sed?
+                              </p>
+                              <p className="prices_servicies">$ 100.000.00</p>
+                         </div>
+
+                         <div className="button_servicies_wrapper">
+                              <button className="button_servicies">Reserva</button>
+                         </div>
+
+                    </section>
+              
+               </section>
+
+               <section className="servicie_wrapper">
+                    <figure className="container_images_servicies">
+                         <figure className="emoticons_servicies">
+
+                              <img src={emoticon} alt="emoticon" />
+                    
+                         </figure>
+                    
+                         <h2 className="subtittle">TERAPIA CHINA</h2>
 
                          <figure className="image_servicios_wrapper">
                               <img src={reiki} alt="Servicios" />
